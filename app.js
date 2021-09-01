2677c054bd067bbcf9d141e7f56957b50f138506
Khayrul Islam <khayrulislam2525@gmail.com>
2021-09-02 03:37:09 +0600

    responsive design added and error solved

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const getBooks = () => {
     const inputField = document.getElementById('inputField');
     const inputValue = inputField.value;
     if(!inputField.value){
-        error.innerText = 'Please search By books name'
-        booksContainer.innerHTML = ''
+        error.innerText = 'Please search By books name';
+        booksContainer.innerHTML = '';
         return;
     }
     //getting data by fetch
@@ -17,7 +17,7 @@ const getBooks = () => {
         .then(res => res.json())
         .then(data => displayBook(data))
         .finally(() => {
-            inputField.value = ''
+            inputField.value = '';
         })
 }
 
@@ -26,20 +26,20 @@ document.getElementById('searchBtn').addEventListener('click', getBooks);
 //showing data on website
 const displayBook = (data) => {
     const books = data.docs;
-    error.innerText = `${books.length} matching results`
+    error.innerText = `${books.length} matching results`;
     if(books.length === 0){
-        error.innerText = 'No matching result found'
-        booksContainer.innerHTML = ''
+        error.innerText = 'No matching result found';
+        booksContainer.innerHTML = '';
     }
     let html = '';
     books.slice(0, 30).forEach(book => {
         html += `
             <div class="book">
-                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" alt="cover image">
+                <img src="https://covers.openlibrary.org/b/id/${book.cover_i || '8194481'}-M.jpg" alt="cover image">
                 <h2>${book.title}</h2>
-                <h3>${book.author_name[0]}</h3>
-                <p>Published in ${book.first_publish_year}</p>
-                <p>Published by ${book.publisher[0]}</p>
+                <h3>${book.author_name?.[0] || ''}</h3>
+                <p>Published in ${book?.first_publish_year || ''}</p>
+                <p>Published by ${book.publisher?.[0] || ''}</p>
             </div>
         `
         booksContainer.innerHTML = html;
